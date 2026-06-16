110fd751444a89fc40e1453dbaca10823ab03e8f
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-06-16 14:17:00 +0200

    Made Install Script Remote Installer

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/bash
 
+URL=https://github.com/TiJ-code/GNOME-workspace-extension.git
+CLONE_DIR="___git_GNOME-workspace-extension.git"
+
+echo "Cloning repository $URL"
+mkdir "$CLONE_DIR"
+git clone "$URL" "$CLONE_DIR"
+cd "$CLONE_DIR"
+
 UUID=$(jq -r '.uuid' metadata.json)
 
 echo "Installing GNOME extension $UUID"
 
-EXT_DIR = ~/.local/share/gnome-shell/extensions/$(UUID)
+EXT_DIR="~/.local/share/gnome-shell/extensions/$UUID"
 
 rm -fr "$EXT_DIR"
 mkdir -p "$EXT_DIR"
 cp extension.js metadata.json "$EXT_DIR"
+
+cd ..
+
+rm -fr "$CLONE_DIR"
 
